f3efaf19aef69e2a981f0b003f008517c2861051
Vagesh36 <vageshvageshpatil@gmail.com>
2026-08-28 23:48:07 +0530

    Add OOP Week 3 solutions

---
+++ src/Main/java/oop/assigment_problems/PositiveNegativeZero.java
@@ -0,0 +1,27 @@
+package Main.java.oop.assigment_problems;
+import java.util.Scanner;
+
+public class PositiveNegativeZero {
+
+    static void classifyNumber(int number) {
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int number = scanner.nextInt();
+
+        classifyNumber(number);
+
+        scanner.close();
+    }
+}
